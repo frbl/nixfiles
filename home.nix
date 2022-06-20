90c9b1217a8233d4dbea5d185d61888742d26d8f
Frank Blaauw <f.j.blaauw@researchable.nl>
2022-06-20 21:36:45 +0200

    chore: added packages

--- home.nix
+++ home.nix
@@ -6,6 +6,30 @@
   home.username = "frbl";
   home.homeDirectory = "/home/frbl";
 
+  # Packages to install
+  home.packages = [
+    pkgs.htop
+    pkgs.curl
+    pkgs.neovim
+    pkgs.xautolock
+    pkgs.fzf
+    pkgs.htop
+    pkgs.patch
+    pkgs.git
+    pkgs.rofi
+    pkgs.silver-searcher
+    pkgs.feh
+    pkgs.xclip
+    pkgs.zathura
+    pkgs.ranger
+    pkgs.python3
+    pkgs.fail2ban
+    pkgs.asdf-vm
+    pkgs.zsh
+    pkgs.tmux
+    pkgs.i3lock
+  ];
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
